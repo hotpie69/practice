357918943a39e4d2a55f89d81f81ccebce593b0b
Minh Đăng <dang.pm172999@sis.hust.edu.vn>
2019-11-13 18:44:04 +0700

    Initial commit

---
+++ src/com/bksoftwarevn/tutorial/bai1/Var.java
@@ -0,0 +1,29 @@
+package com.bksoftwarevn.tutorial.bai1;
+
+public class Var {
+    public static void main(String[] args) {
+        // khai bao bien
+        // datatype name_var [= val];
+
+        int a;
+        //byte, short
+        // long
+        int b = 10;
+        int c = b;
+        float d = 7;
+        float e = 9f;
+        float m = 10F;
+        double k = 11d;
+        char l = 'c';
+        boolean o = false;
+        {
+
+        }
+
+
+        System.out.println(c);
+
+    }
+            System.out.println(c);
+
+}
